9ea5a26b1877107206fecc72c2eb66565fede05b
Sriteja Sugoor <sritejakittu777@gmail.com>
2022-06-09 22:21:32 +0530

    added files

---
+++ app/src/main/java/com/apibotdeveloper/passpin/FailedActivity.java
@@ -0,0 +1,14 @@
+package com.apibotdeveloper.passpin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failed);
+    }
+}
--- app/src/main/java/com/apibotdeveloper/passpin/MainActivity.java
+++ app/src/main/java/com/apibotdeveloper/passpin/MainActivity.java
@@ -3,14 +3,21 @@ package com.apibotdeveloper.passpin;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.KeyguardManager;
 import android.content.Intent;
+import android.graphics.ImageFormat;
 import android.hardware.biometrics.BiometricManager;
 import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
 import android.os.CancellationSignal;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import static android.hardware.biometrics.BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE;
+import static android.hardware.biometrics.BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED;
+import static android.hardware.biometrics.BiometricManager.BIOMETRIC_SUCCESS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +28,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        ImageFormat
+    }
+
+    public void checkDeviceSecure(View view) {
+        String way = view.getTag().toString();
+        Log.d(TAG, "Checking device secure " + way);
+        Boolean secure = false;
+        if (way.equals("km")) {
+            KeyguardManager keyguardManager = (KeyguardManager) this.getSystemService(KEYGUARD_SERVICE);
+            secure = (keyguardManager.isDeviceSecure() && keyguardManager.isKeyguardSecure());
+        } else if (way.equals("bm")) {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                BiometricManager biometricManager = (BiometricManager) this.getSystemService(BIOMETRIC_SERVICE);
+                secure = (BIOMETRIC_SUCCESS == biometricManager.canAuthenticate());
+                Log.d(TAG, "start");
+                Log.d(TAG, BIOMETRIC_SERVICE);
+                Log.d(TAG, String.valueOf(BIOMETRIC_SUCCESS));
+                Log.d(TAG, String.valueOf(BIOMETRIC_ERROR_NONE_ENROLLED));
+                Log.d(TAG, String.valueOf(biometricManager.canAuthenticate()));
+                Log.d(TAG, String.valueOf(BIOMETRIC_ERROR_HW_UNAVAILABLE));
+                Log.d(TAG, String.valueOf(biometricManager.canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL)));
+                Log.d(TAG, "stop");
+
+            } else {
+                Toast.makeText(getApplicationContext(), "android version not supported", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        if (secure) {
+            Toast.makeText(getApplicationContext(), "Device Secure", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Device Not Secure", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
     public void enterPasswordAndEnterHeaven(View view) {
         Log.d(TAG, "Process started for entering hell");
         KeyguardManager keyguardManager = (KeyguardManager) this.getSystemService(KEYGUARD_SERVICE);
-        if (keyguardManager.isDeviceSecure() && keyguardManager.isKeyguardSecure()) {
+        String checkSecure = view.getTag().toString();
+        if (checkSecure.equals("yes")) {
+            if (!(keyguardManager.isDeviceSecure() && keyguardManager.isKeyguardSecure())) {
+                Log.d(TAG, "Pathway not secure, please apply a setting with GOD (james)");
+                Toast.makeText(getApplicationContext(), "Pin not set", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        } else {
+            Log.d(TAG, "Trying to access even if it not secure");
+        }
             Log.d(TAG, "Pathway is secure, proceeding");
 
             if (keyguardManager.inKeyguardRestrictedInputMode()) {
@@ -60,12 +111,22 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, "You fingerprint will be the sacrifice");
                         startActivity(new Intent(MainActivity.this, HellActivity.class));
                     }
+
+                    @Override
+                    public void onAuthenticationFailed() {
+                        super.onAuthenticationFailed();
+                        Log.d(TAG, "Authentication failed");
+                        Toast.makeText(getApplicationContext(), "Auth failed", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, FailedActivity.class));
+                    }
                 });
             }
 
-        } else {
-            Log.d(TAG, "Pathway not secure, please apply a setting with GOD (james)");
-        }
+//        } else {
+//            Log.d(TAG, "Pathway not secure, please apply a setting with GOD (james)");
+//            Toast.makeText(getApplicationContext(), "Pin not set", Toast.LENGTH_SHORT).show();
+//
+//        }
     }
 
     @Override
@@ -74,11 +135,14 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "Verifying your prayers - only non-barter prayers will be heard - its' not a wish granting system " + requestCode + " " + resultCode);
 
-        if (requestCode == INTENT_AUTHENTICATE) {
+        if (requestCode == INTENT_AUTHENTICATE && resultCode == Activity.RESULT_OK) {
             Log.d(TAG, "Congratulation human! you are officially a member of the Hell organization");
             startActivity(new Intent(MainActivity.this, HellActivity.class));
         } else {
             Log.d(TAG, "Buzz off poser");
+            Log.d(TAG, "Authentication failed");
+            Toast.makeText(getApplicationContext(), "Auth failed", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, FailedActivity.class));
         }
     }
 }
